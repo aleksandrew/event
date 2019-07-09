4f6cbe0aa8cf5db4e77cd217f8a79c1e0d5f3acb
Andrey Aleksandrov <andrew.aleksandrow97@gmail.com>
2019-07-09 17:22:42 +0300

    Data reset

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,9 +1,1 @@
 import $ from 'jquery';
-
-$(() => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('%c App loaded meta', 'color: #fff; background: #ff0000; font-size: 22px;font-weight: bold;',
-      '\n process.env.NODE_ENV:', process.env.NODE_ENV,
-    );
-  }
-});
